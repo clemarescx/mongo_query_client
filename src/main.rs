3496ee906a0be9120d2c8cf252446e7773bb5228
Clément Marescaux <cmarescaux@hotmail.com>
2018-12-20 11:08:53 +0100

    Fix: flush prompt to stdout and clean string buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use mongodb::db::ThreadedDatabase;
 use mongodb::{bson, doc, Bson, Client, ThreadedClient};
 use serde_derive::*;
+use std::io::{self, Write};
 
 const CONFIG_FILEPATH: &'static str = "db_connection.json";
 
@@ -17,31 +18,45 @@ fn main() {
     let client = Client::connect(&db_config.url, db_config.port)
         .expect("Failed to connect client to database");
 
-    let mut db_name = String::new();
-    let mut coll_name = String::new();
+    let mut db_name_buffer = String::new();
+    let mut coll_name_buffer = String::new();
 
-    // read from stdin not working
-    println!("db name: ");
-    std::io::stdin()
-        .read_line(&mut db_name)
-        .expect("could not read from stdin");
-    println!("collection name: ");
-    std::io::stdin()
-        .read_line(&mut coll_name)
-        .expect("could not read from stdin");
+    read_from_stdin("db name: ", &mut db_name_buffer);
+    read_from_stdin("collection name: ", &mut coll_name_buffer);
+
+    let db_name = if db_name_buffer.trim().is_empty() {
+        db_config.default_db
+    } else {
+        db_name_buffer.trim().to_owned()
+    };
+    let coll_name = if coll_name_buffer.trim().is_empty() {
+        db_config.default_collection
+    } else {
+        coll_name_buffer.trim().to_owned()
+    };
 
     let coll = client.db(&db_name).collection(&coll_name);
-
-    println!("collection found.");
 
     let cursor = coll.find(None, None).expect("could not create a cursor");
     let total = cursor.count();
 
-    println!("Total number of steps: {}", total);
+    println!("Total number of entries: {}", total);
+}
+
+fn read_from_stdin(prompt: &str, buffer: &mut String) {
+    print!("{}", prompt);
+    io::stdout()
+        .flush()
+        .expect("could not print prompt to stdout");
+    io::stdin()
+        .read_line(buffer)
+        .expect("could not read from stdin");
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct ServerConnectionConfig {
     url: String,
     port: u16,
+    default_db: String,
+    default_collection: String,
 }
